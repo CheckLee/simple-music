d025e259c055bdab30326ace9292bff770d55d6c
CheckLee <liyanliang1993@163.com>
2018-03-25 22:11:15 +0800

    test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="test"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .test {
+    width: 100px;
+    height: 100px;
+    border: 1px solid #cccccc;
+    color: gold;
+  }
+</style>
