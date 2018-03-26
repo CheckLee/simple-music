70f50e02385f4dbebdcf0a554e6a36f22da3138a
JiangWeixian <saber_wei@163.com>
2018-03-26 21:28:54 +0800

    init collect-item

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="ITEM" class="collect-item">
-    <p class="collect-item-header"><i class="material-icons">email</i></p>
-    div.collect-item
+    <p class="collect-item-header"><i class="material-icons md-48">email</i></p>
+    <div class="collect-item-body"></div>
+    <p class="collect-item-tail"><i class="material-icons md-48">keyboard_arrow_right</i></p>
   </div>
 </template>
 
 <script>
     export default {
-        name: "account"
+        name: "Account"
     }
 </script>
 
 <style scoped>
   .collect-item {
+    width: 100%;
     display: flex;
+    flex-wrap: nowrap;
   }
-
-
+  .collect-item > * {
+    /*border: 1px solid red;*/
+  }
+  .collect-item-header, .collect-item-tail {
+    text-align: left;
+  }
+  .collect-item-header {
+    flex-grow: 0.5;
+    /*text-indent: 30px;*/
+  }
+  .collect-item-tail {
+    flex-grow: 0.5;
+  }
+  .collect-item-body {
+    flex-grow: 8;
+  }
 </style>
