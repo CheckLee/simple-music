5a923f70242d7f5b5918f257ca85a2ac8d29fad5
JiangWeixian <saber_wei@163.com>
2018-03-26 19:19:48 +0800

    add account

---
+++ src/components/Account.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="ITEM" class="collect-item">
+    <p class="collect-item-header"><i class="material-icons">email</i></p>
+    div.collect-item
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "account"
+    }
+</script>
+
+<style scoped>
+  .collect-item {
+    display: flex;
+  }
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Account from '@/components/account'
+import Account from '@/components/Account'
 
 Vue.use(Router)
 
