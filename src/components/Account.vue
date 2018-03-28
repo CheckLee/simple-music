f79889c86ee176355ab94fda5c2e1f72806b3cf0
JiangWeixian <saber_wei@163.com>
2018-03-28 21:19:34 +0800

    collect-item complete

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,45 +1,18 @@
 <template>
-  <div id="ITEM" class="collect-item">
-    <p class="collect-item-header"><i class="material-icons md-48">email</i></p>
-    <p class="collect-item-body">邮件</p>
-    <p class="collect-item-tail"><i class="material-icons md-48">keyboard_arrow_right</i></p>
+  <div id="ACCOUNT" class="account">
+    <collect-item header-icon-name="email" item-name="邮件" tail-icon-name="keyboard_arrow_right" item-class="text"></collect-item>
   </div>
 </template>
 
 <script>
+    import CollectItem from "./base/CollectItem/CollectItem";
+
     export default {
-      name: "Account",
-      props: ['headerIconType', 'itemName', 'tailIconType']
+      components: {CollectItem},
+      name: "Account"
     }
 </script>
 
 <style scoped>
-  .collect-item {
-    width: 100%;
-    height: 80px;
-    line-height: 80px;
-    font-size: 28px;
-    border-bottom: 1px solid #d8d8d8;
-    margin-left: 30px;
-    display: flex;
-    flex-wrap: nowrap;
-  }
-  .material-icons {
-    height: inherit;
-    line-height: inherit;
 
-  }
-  .collect-item-header, .collect-item-tail {
-    text-align: left;
-  }
-  .collect-item-header {
-    width: 48px;
-  }
-  .collect-item-tail {
-    width: 78px;
-  }
-  .collect-item-body {
-    width: calc(100% - 156px);
-    text-indent: 5px;
-  }
 </style>
---
+++ src/components/base/CollectItem/CollectItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="collect-item" :class="collectItemClassName">
+    <p class="collect-item-header"><i class="material-icons md-48">{{ headerIconName }}</i></p>
+    <p class="collect-item-body">{{ itemName }}</p>
+    <p class="collect-item-tail"><i class="material-icons md-48">{{ tailIconName }}</i></p>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "collect-item",
+      props: ['headerIconName', 'itemName', 'tailIconName', 'itemClass'],
+      computed: {
+        collectItemClassName() {
+          return this.itemClass+'-collect-item'
+        }
+      }
+    }
+</script>
+
+<style type="text/stylus" lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "./CollectItem.styl"
+</style>
+
+
