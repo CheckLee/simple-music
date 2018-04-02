1b741033501c3c58687c7f0a4525147770de8c97
JiangWeixian <saber_wei@163.com>
2018-04-02 18:36:06 +0800

    img-viewer in processing

---
+++ src/components/base/Button/FloatButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="float-button">
+    <span>
+      <i class="material-icons md-light">{{ buttonIconName }}</i>
+    </span>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "float-button",
+      data() {
+        return {
+          buttonIconName: 'create'
+        }
+      }
+    }
+</script>
+
+<style type="text/stylus" lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "./FloatButton.styl"
+</style>
